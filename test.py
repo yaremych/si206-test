110de6a9d69c64e4b1c7a79a1e665c902e229ee8
Yu-Jen Lin <peacedove5210@gmail.com>
2017-01-25 19:35:08 -0500

    add test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+def fab(num):
+  if (num == 1 or num == 2):
+    return 1
+  return (fab(num - 1) + fab(num - 2))
+
+print(fab(1))
+print(fab(2))
+print(fab(3))
+print(fab(4))
+print(fab(5))
+print(fab(6))
